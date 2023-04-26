025b5e74cba924e6b5322896ea2477de969ef2e7
savinims <savini.ms@gmail.com>
2023-04-26 16:05:06 -0600

    'ingestor_logic'

---
+++ src/QuoteEngine/Ingestor.py
@@ -0,0 +1,17 @@
+from typing import List
+from .IngestorInterface import IngestorInterface
+from .QuoteModel import QuoteModel
+from .DocxIngestor import DocxIngestor
+from .CSVIngestor import CSVIngestor
+from .PDFIngestor import PDFIngestor
+from .TxtIngestor import TxtIngestor
+
+
+class Ingestor(IngestorInterface):
+    importers = [DocxIngestor, CSVIngestor, PDFIngestor, TxtIngestor]
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        for importer in cls.importers:
+            if importer.can_ingest(path):
+                return importer.parse(path)
